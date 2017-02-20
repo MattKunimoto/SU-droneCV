ac11849cd890dedc174f6fc858598b8d3ad2c02f
ErikvanG <ErikvanG@users.noreply.github.com>
2017-02-19 17:44:40 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,29 +1,102 @@
-//Version 0.1 Drone CV
-//General framework of how OpenCV works in C, needs RaspiCam additions:
-// https://www.uco.es/investiga/grupos/ava/node/40
+//Version 1 Drone CV
 //
 
 #include </usr/local/include/opencv2/core/core.hpp>
 #include </usr/local/include/opencv2/highgui/highgui.hpp>
 #include </usr/local/include/opencv2/imgproc/imgproc.hpp>
+#include "raspicam_cv"
+#include "private/private_impl.h"
+#include "scaler.h"
 #include <stdio.h>
 #include <thread>
 #include <mutex>
+#include <lock>
+#include <condition_variable>
+#include <vector>
 
-std::mutex bufferUse;
+//Declare chars for different sides
+static const char BACK = 'b';
+static const char SIDE = 's';
+static const char FRONT = 'f';
 
-void cameraFeed(cv::Mat& buffer)
+//CascadeClassifiers
+CascadeClassifier backCascade;
+CascadeClassifier frontCascade;
+CascadeClassifier sideCascade;
+
+//Paths to classifiers
+static const string backCascadeName = "/home/pi/seniordesign/classifiers/back/classifier.xml";
+static const string frontCascadeName = "/home/pi/seniordesign/classifiers/front/classifier.xml";
+static const string sideCascadeName = "/home/pi/seniordesign/classifiers/side/classifier.xml";
+
+//Vectors that contain detection location info
+std::vector<cv::Rect> backPat;
+std::vector<cv::Rect> frontPat;
+std::vector<cv::Rect> sidePat;
+
+//Threading primitives
+std::mutex bufferMtx;
+std::condition_variable runClassifiers;
+std::mutex classifierMtx;
+std::condition_variable classifierFinished;
+
+
+cv::Mat buffer;
+
+raspicam::RaspiCam_Cv camera;
+
+bool running;
+
+class locSizeSide
 {
-	//import camera feed and save image to sd card
-	bufferUse.lock();
-	buffer = cv::imread("pic.jpg");
-	bufferUse.unlock();
+public:
+	locSizeSide::locSizeSide()
+	{
+		X = 0;
+		Y = 0;
+		W = 0;
+		H = 0;
+		Side = 0;
+	}
+
+	locSizeSide::locSizeSide(int x, int y, int w, int h, char side)
+	{
+		X = x;
+		Y = y;
+		W = w;
+		H = h;
+		Side = side;
+	}
+	int X, Y, W, H;
+	char Side;
 }
 
-void harrisCorners(cv::Mat& buffer)
+void cameraFeed(raspicam::RaspiCam_Cv& camera)
+{
+	//import camera feed and save image to sd card
+	while(running){
+	/*	bufferUse.lock();
+		buffer = cv::imread("pic.jpg");
+		bufferUse.unlock();
+	*/
+		camera.grab();
+	}
+}
+
+void grabImageFromCamera()
+{
+	while(running){
+		std::lock_guard<std::mutex> bufferLock(bufferMtx);
+		camera.retrieve(buffer);
+		equalizeHist(buffer, buffer);
+		runClassifiers.notify_all();
+	}
+}
+
+void frontClassifier(raspicam::RaspiCam_Cv& camera, cv::Mat& buffer, locSizeSide::locSizeSide& positionInfo)
 {
 	//swap image through pipe like object or locks and pass by reference
-	cv::Mat img, gray, harris, harris_norm, harris_norm_scaled;
+	/*cv::Mat img, gray, harris, harris_norm, harris_norm_scaled;
 	bufferUse.lock();
 	img = buffer;
 	bufferUse.unlock();
@@ -52,21 +125,39 @@ void harrisCorners(cv::Mat& buffer)
 	cv::imshow("Display Image", img);
 
 	cv::waitKey(0);
+	*/
+	std::unique_lock<std::mutex> bufferLock(bufferMtx);
+	runClassifiers.wait();
+	frontCascade.detectMultiScale(buffer, backPat, 1.1, 2, 0, cv::Size(10, 10));
+	for(int i = 0; i < backPat.size())
+
 }
 
 int main( void )
 {
-	// 1. initialize camera and shit
+	// 1. initialize camera and create data containers
 	// 2. call thread of cameraFeed
 	// 3. call thread of harris corners
 
-	cv::Mat buffer;
+ //create camera
+	//setup camera settings
+	camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
+	camera.open(1); //initialize camera and start capturing
 
-	std::thread camFeed(cameraFeed, std::ref(buffer));
-	std::thread harris(harrisCorners, std::ref(buffer));
+
+	locSizeSide::locSizeSide skater;
+
+	bool running = true;
+
+	std::thread camFeed(cameraFeed, std::ref(camera), std::ref(buffer), running);
+	std::thread bufferSwap(grabImageFromCamera, std::ref(camera), buffer);
+	std::thread backClassifier(backClassifier, std::ref(buffer), skater);
+	std::thread sideClassifier(sideClassifier, std::ref(buffer), skater);
+	std::thread frontClassifier(frontClassifier, std::ref(buffer), skater);
 
 	camFeed.join();
 	harris.join();
 
+	camera.release();
 	return 0;
 }
